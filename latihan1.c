fd9ec28fcb748ed2b1fc5d06174f9d9534468c6b
illhammf <illhammf@users.noreply.github.com>
2026-08-01 18:04:17 +0700

    add latihan1

---
+++ latihan1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // Memasukkan library standar agar kita bisa menggunakan fungsi-fungsi di dalamnya.
+
+int main() {
+
+    printf("===== Biodata =====\n"); // Fungsi printf() digunakan untuk menampilkan teks ke layar. Dalam hal ini, teks yang ditampilkan adalah "===== Biodata =====".
+    printf("Nama: Ilham Firmansyah\n"); // Fungsi printf() digunakan untuk menampilkan teks ke layar. Dalam hal ini, teks yang ditampilkan adalah "Nama: Ilham Firmansyah".
+    printf("Umur: 20 Tahun\n"); // Fungsi printf() digunakan untuk menampilkan teks ke layar. Dalam hal ini, teks yang ditampilkan adalah "Umur: 20 Tahun".
+    printf("Kampus: Universitas Esa Unggul\n"); // Fungsi printf() digunakan untuk menampilkan teks ke layar. Dalam hal ini, teks yang ditampilkan adalah "Kampus: Universitas Esa Unggul".
+
+    return 0; // Mengembalikan nilai 0 untuk menandakan bahwa program telah selesai dijalankan dengan sukses.
+
+}
