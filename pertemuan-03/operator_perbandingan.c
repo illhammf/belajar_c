80063542b2a27d8ff72ef96350b3dc95bfa0a363
illhammf <illhammf@users.noreply.github.com>
2026-08-02 14:36:36 +0700

    update operator perbandingan

---
+++ pertemuan-03/operator_perbandingan.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    printf("=== OPERATOR PERBANDINGAN ===\n\n");
+
+    printf("a = %d\n", a);
+    printf("b = %d\n\n", b);
+
+    printf("a == b : %d\n", a == b);
+    printf("a != b : %d\n", a != b);
+    printf("a > b  : %d\n", a > b);
+    printf("a < b  : %d\n", a < b);
+    printf("a >= b : %d\n", a >= b);
+    printf("a <= b : %d\n", a <= b);
+
+    return 0;
+}
