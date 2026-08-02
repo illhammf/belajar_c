01b71f7fb7a78b4dc0b7417c372587b31e292888
illhammf <illhammf@users.noreply.github.com>
2026-08-02 13:48:25 +0700

    add varibel sebelum dan sesudah

--- pertemuan-02/variabel.c
+++ pertemuan-02/variabel.c
@@ -8,3 +8,20 @@ int main()
 
     return 0;
 }
+
+// atau bisa juga seperti ini:
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int umur = 20;
+
+//     printf("Sebelum diubah : %d\n", umur);
+
+//     umur = 21;
+
+//     printf("Sesudah diubah : %d\n", umur);
+
+//     return 0;
+// }
