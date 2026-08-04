cb87e6ab76ed4766746db5b2096112c6e9b8a740
illhammf <illhammf@users.noreply.github.com>
2026-08-04 20:25:44 +0700

    update for loop

---
+++ pertemuan-06/for_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("=== PERULANGAN FOR ===\n\n");
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("Perulangan ke-%d\n", i);
+    }
+
+    return 0;
+}
