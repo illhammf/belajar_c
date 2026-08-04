d99b93be57f76c0409fc7f3613422ea5c3fa419a
illhammf <illhammf@users.noreply.github.com>
2026-08-04 20:35:02 +0700

    update infinite loop

---
+++ pertemuan-06/infinite_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Infinite loop adalah salah satu jenis perulangan yang digunakan untuk mengeksekusi blok kode tertentu beberapa kali tanpa batas. Perulangan ini biasanya digunakan ketika kita ingin menjalankan program secara terus-menerus hingga dihentikan secara manual. Sintaks dasar dari infinite loop adalah sebagai berikut:
+int main()
+{
+    while (1)
+    {
+        printf("Program berjalan terus...\n");
+    }
+
+    return 0;
+}
