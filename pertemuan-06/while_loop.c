2cd025f3f71eaec44ce0a8ff13878b2d3f725098
illhammf <illhammf@users.noreply.github.com>
2026-08-04 20:30:17 +0700

    update while loop

---
+++ pertemuan-06/while_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    int i = 1;
+
+    printf("=== PERULANGAN WHILE ===\n\n");
+
+    while (i <= 10) {
+        printf("Perulangan ke-%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
