614e6ade86e6f3cb9f519d299151630dee076629
illhammf <illhammf@users.noreply.github.com>
2026-08-07 06:13:44 +0700

    update string dasar

---
+++ pertemuan-08/string_dasar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// String adalah tipe data array yang berisi karakter-karakter. String selalu diakhiri dengan karakter null '\0' untuk men
+int main()
+{
+    char nama[] = "Ilham Firmansyah";
+
+    printf("=== STRING DASAR ===\n\n");
+    printf("Nama : %s\n", nama);
+
+    return 0;
+}
